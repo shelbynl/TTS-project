1f1be622a197ad4e97061e73e18141a1e0d6e7c1
Thaddeus Thomas <thomasthaddeus@cityuniversity.edu>
2023-02-24 15:58:53 -0700

    exe working, no output

--- TextToSpeech/easy_gui.py
+++ TextToSpeech/easy_gui.py
@@ -1,10 +1,14 @@
 """Creates the box for our executable"""
 
 import os
+import os.path
 import easygui
-import synthesizer as synth
 
-x = synth
+
+p = './synthesizer.py'
+
+path = os.path.relpath(p, start=os.curdir)
+
 MSG = "Load application..."
 TITLE="Text-to-Speech Audio ReImagination" # TSAR I
 CHOICES = ["Create Audio File",".","Exit"]
@@ -21,9 +25,9 @@ def main():
     """runs the main application"""
     reply = easygui.buttonbox(MSG, TITLE, CHOICES)
     if reply == CHOICES[0]:
-        return synth
+        return path
     if reply == "CHOICES[1]":
-        num1 = os.startfile(f"{synth}")
+        num1 = os.startfile(f"{path}")
         return num1
     else:
         os.system("Exit")
